980153988c264419d6d5af1b8771f55f24476cc9
jan-57 <j.m.mch.005@pm.me>
2026-02-02 23:30:22 +0100

    Selection Box

--- LODgenerator.py
+++ LODgenerator.py
@@ -9,14 +9,34 @@ class UI_Panel(bpy.types.Panel): # case sensitive - not panel but Panel
     
     def draw(self, context ):
         _layout = self.layout
+#        _row = _layout.row()     - With row, everything is in the same line? Separator didnt work to do a break 
+        
+        _layout.label(text="Select an Object to create an LOD,")
+#       _layout.separator()       -  This creates a break in the UI text 
+        _layout.label(text="Then select how many LOD's you want")
+        _layout.label(text="Then Finnaly press the button Start")
+        
+        _data = context.scene.my_tool    #grabs the stuff inside of my_tool
+        _layout.prop(_data, "targetSelection")   # draws the box 
         
         
         
-def register():                            # register the panels 
+        
+class SelectionBox(bpy.types.PropertyGroup):  
+        targetSelection: bpy.props.PointerProperty(   # targetSelection -- bpy.props.PointerProperty = the water drop thing to select the object 
+        name="Select Object for an LOD",    # display text in that box 
+        type=bpy.types.Object
+    )        
+        
+def register():                            # register the classes / We got to register every class 
     bpy.utils.register_class(UI_Panel)
+    bpy.utils.register_class(SelectionBox)
+    bpy.types.Scene.my_tool = bpy.props.PointerProperty(type=SelectionBox)   # saves the picked object 
     
-def unregister():                          # unregister the panels 
+def unregister():                          # unregister the class / reverse of registering text  
     bpy.utils.unregister_class(UI_Panel)
+    bpy.utils.unregister_class(SelectionBox)
+    del bpy.types.Scene.my_too
     
 if __name__ == "__main__":                 # exec the script
     register()
